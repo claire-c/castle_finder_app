6e0854b2d34d7e74cea851405205c950ced81aea
Matthew Thorne <mwilsonthorne@gmail.com>
2018-06-04 16:32:15 +0100

    model can retrieve items from database

--- client/src/app.js
+++ client/src/app.js
@@ -6,12 +6,13 @@ const Castles = require('./models/castles.js');
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello');
 
-const form = window.document.querySelector('#form');
-const formView = new FormView(form);
-formView.setUpFormListener();
+  const form = window.document.querySelector('#form');
+  const formView = new FormView(form);
+  formView.setUpFormListener();
 
-const castles = new Castles();
-castles.subscribeToFormView();
+  const castles = new Castles();
+  castles.subscribeToFormView();
+  castles.getDataAndReturnFromOurCastleAPI();
 
 
 });
--- client/src/models/castles.js
+++ client/src/models/castles.js
@@ -1,4 +1,5 @@
 const PubSub = require('../helpers/pub_sub.js');
+const Request = require('../helpers/request.js');
 
 // // TODO: add in parameter to take the map api here later.
 const Castles = function(){
@@ -14,6 +15,19 @@ Castles.prototype.subscribeToFormView = function () {
   })
 
 };
+
+// request all the castle data from the server
+Castles.prototype.getDataAndReturnFromOurCastleAPI = function () {
+  const url = 'http://localhost:3000/api/castleInfo';
+  const request = new Request(url);
+  request.get()
+    .then((allCastleData) => {
+      console.log('Returned from server:', allCastleData);
+    })
+
+
+};
+// log out the response
 
 
 
